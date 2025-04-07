bcb86071403268e674789726590ca0e3aaad68b8
kanielv <kanielvicencio@gmail.com>
2025-04-07 19:08:13 +0000

    feat: changed meta data tag and moved signin layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'skinple',
+  description: 'simple way to create customized skin routines',
 };
 
 const geistSans = Geist({
@@ -31,8 +31,8 @@ export default function RootLayout({
   return (
     <html lang='en' className={geistSans.className} suppressHydrationWarning>
       <body className='bg-background text-foreground'>
-        <main>{children}</main>
         <HeaderAuth />
+        <main>{children}</main>
       </body>
     </html>
   );
