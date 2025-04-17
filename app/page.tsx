f5b48b1820476e8f921b635adac422ab198e24de
fbigabig <fbigabig@gmail.com>
2025-04-16 22:22:50 -0400

    Prototype home pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default async function Home() {
   return (
     <div className='flex w-full flex-1 flex-col gap-8 leftspace2 topspace'>
     <h3> <b>Welcome to Skinple!</b></h3>
-    <a href='/protected/quiz' className='link'>Take the quiz to find products recommended for you! </a>
-    <a href='/protected/list-maker' className='link'>Build your own list of skincare products with our listmaker!</a>
+
     <img 
       src='./app/new-high-glass-glass-bottle-came.png'
       alt="skincare bottles" 
@@ -18,8 +17,6 @@ export default async function Home() {
     <h1 style={{color:'#ffb5a7'}}> We believe everyone should be able to easily find skincare that works for them!</h1>
     </div>
     <br/>
-    <div className='button'>
-    <b><a href='/protected/quiz'>Retake the quiz!</a></b></div>
     
     </div>
   );
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -4,6 +4,7 @@ import { InfoIcon } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import { getUserAction } from '@/app/protected/actions';
 import './../homepage.css';
+import pic from './../new-high-glass-glass-bottle-came.png';
 export default async function ProtectedPage() {
   const supabase = await createClient();
 
@@ -23,22 +24,29 @@ export default async function ProtectedPage() {
   }
 
   return (
+    <div>
     <div className='flex w-full flex-1 flex-col gap-8 leftspace2 topspace'>
     <h3> <b>Welcome back to Skinple!</b></h3>
     <a href='/protected/recommendations' className='link'>Check out products recommended for you! </a>
     <a href='/protected/list-maker' className='link'>Build your own list of skincare products with our listmaker!</a>
-    <img 
-      src='./app/new-high-glass-glass-bottle-came.png'
-      alt="skincare bottles" 
-      style={{ float: 'right', marginLeft: '10px' }} 
-    />
+    </div>
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <img 
+        src={pic.src}
+        alt="skincare bottles" 
+        style={{ height: 'auto', width: '400px' }} 
+      />
+    </div>
+    <div className='flex w-full flex-1 flex-col gap-8 leftspace2 topspace'>
+
     <div className='half'>
     <h1 style={{color:'#ffb5a7'}}> We believe everyone should be able to easily find skincare that works for them!</h1>
     </div>
     <br/>
     <div className='button'>
     <b><a href='/protected/quiz'>Retake the quiz!</a></b></div>
-    
     </div>
+    </div>
+    
   );
 }
